d70ee5ea7d11200f6bf82423673df98243dba348
KarthikKumaran S <gsailappanskarthikkumaran@gmail.com>
2026-07-14 17:10:22 +0530

    add change

--- mobile/capacitor.config.ts
+++ mobile/capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'mobile',
-  webDir: 'dist/language_app/browser'
+  webDir: 'dist/language_app/browser',
+  server: {
+    // androidScheme: 'https',
+    cleartext: true
+  }
 };
 
 export default config;
