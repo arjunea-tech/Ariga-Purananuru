54100313769d7db8b1c0ad0c43d74a952dfc7af3
Logesh <24pca150@anjaconline.org>
2026-07-15 11:31:36 +0530

    changes

--- mobile/capacitor.config.ts
+++ mobile/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'mobile',
+  appName: 'தமிழ் யாப்பு',
   webDir: 'dist/language_app/browser',
   server: {
     androidScheme: 'https',
