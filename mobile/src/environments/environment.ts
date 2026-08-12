adaee50705124d6d1986d7b24f6fe40cc98313fb
Logesh <24pca150@anjaconline.org>
2026-08-12 16:21:11 +0530

    environment changed

--- mobile/src/environments/environment.ts
+++ mobile/src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
-  // apiUrl: 'https://azhagutamil.com/api',
-  // baseUrl: 'https://azhagutamil.com',
+  apiUrl: 'https://azhagutamil.com/api',
+  baseUrl: 'https://azhagutamil.com',
   //   apiUrl: 'https://web-production-077c6.up.railway.app/api',
   //   baseUrl: 'https://web-production-077c6.up.railway.app',
-  apiUrl: 'http://localhost:8000/api',
-  baseUrl: 'http://localhost:8000'
+  // apiUrl: 'http://localhost:8000/api',
+  // baseUrl: 'http://localhost:8000'
 };
